9163f6e47dff77d8f49458b0fadd1de57817fc76
Manav <manavpatel871@gmail.com>
2023-08-18 16:16:46 +0530

    incomplete file

--- Lab_Questions/lab-3-part1/part1/dictionary.cpp
+++ Lab_Questions/lab-3-part1/part1/dictionary.cpp
@@ -1,16 +1,34 @@
 #include "dictionary.h"
+#include "cmath"
 
 Dictionary::Dictionary(){
+    A = new Entry[N];
+    for(int i = 0;i < N;i++)
+        {
+            A[i].key = NULL;
+        }
 };
 
 
 int Dictionary::hashValue(char key[]){
     int hashValue = 0;
-    // compute hash
+    long long int poly = 0;
+    int i = 0;
+    while(key[i] != NULL)
+        {
+            poly += (int(key[i]) * pow(31,i)); 
+            i++;
+        }
+    double pol = poly * (sqrt(5) - 1) / 2;
+    pol = pol - int(pol);
+    hashValue = int(floor(N * pol));
     return hashValue;
 }
 
 int Dictionary::findFreeIndex(char key[]){
+    int hash_val = hashValue(key);
+    if(A[hash_val].key == NULL) return hash_val;
+
     return -1;
 }
 
